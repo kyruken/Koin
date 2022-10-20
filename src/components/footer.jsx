b83c00b4ea42e9c6cb8c9168de5f776fa971b102
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-19 19:21:57 -0700

    style footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className="footer">
-            <div className="links">
-                <a>Alternatives</a>
-                <a>About us</a>
-                <a>Socials</a>
+            <div className="margin-right margin-top">
+                <div className="links">
+                    <a>Alternatives</a>
+                    <a>About us</a>
+                    <a>Socials</a>
+                </div>
+                <p>Heemerdingers &copy; Copyright 2022</p>
             </div>
-
-            <p>Heemerdingers &copy; Copyright 2022</p>
         </div>
     )
 }
