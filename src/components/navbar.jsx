ba0017a065cdb561625168c40d81d576a3f9d916
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-19 18:56:29 -0700

    finish styling searchbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,12 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <h1>Coinchange</h1>
-            <img src='' />
-            <input type='text' name="searchbar" id="searchbar" placeholder="search"></input>
+            
+            <div className="searchbar-wrapper">
+            <input type='text' name="searchbar" id="searchbar" placeholder="Search"></input>
+            <img alt="Magnifying glass icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/256px-Magnifying_glass_icon.svg.png"></img>
+            </div>
+            
         </div>
     )
 }
