eb4bcc535de494a0efbe820aef743321aa8467ed
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-19 18:08:54 -0700

    create navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>Coinchange</h1>
+            <img src='' />
+            <input type='text' name="searchbar" id="searchbar" placeholder="search"></input>
+        </div>
+    )
+}
