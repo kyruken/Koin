c42bad7c038ba409721a9b4fd538f8b037ecfa3a
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-19 19:13:31 -0700

    add footer content

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 
 import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      sdsds
+
+
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="links">
+                <a>Alternatives</a>
+                <a>About us</a>
+                <a>Socials</a>
+            </div>
+
+            <p>Heemerdingers &copy; Copyright 2022</p>
+        </div>
+    )
+}
