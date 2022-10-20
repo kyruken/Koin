6bce523e0ec71643842e8b51cf1f7722f2ae9b76
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-19 18:29:08 -0700

    add styles to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Navbar() {
     return (
-        <div>
+        <div className="navbar">
             <h1>Coinchange</h1>
             <img src='' />
             <input type='text' name="searchbar" id="searchbar" placeholder="search"></input>
