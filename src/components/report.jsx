4df6d9ee385d6c96877788debb46704bfca7b10e
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-21 17:44:19 -0700

    style report buttons

--- src/components/report.jsx
+++ src/components/report.jsx
@@ -4,27 +4,28 @@ export default function Report() {
     return (
         <div className="report-container margin-left">
             <h2>Market Report</h2>
-            <div className="column-1-2">
-                <button>All currencies</button>
-                <button>Favorites</button>
-                {/* Maybe we do gainers and losers */}
-                <button>Gainers</button>
-                <button>Losers</button>
-            </div>
 
-            <div className="column-2-2">
-                <select name="currency" id="currency">
-                    <option value="usd">USD</option>
-                    <option value="rupee">Rupee</option>
-                    <option value="won">Won</option>
-
-                </select>
-                <select name="timestamp" id="timestamp">
-                    <option value="day">1D</option>
-                    <option value="week">7D</option>
-                    <option value="month">1M</option>
-                    <option value="year">1Y</option>
-                </select>
+            <div className="report-buttons">
+                <div className="column-1-2">
+                    <button>All currencies</button>
+                    <button>Favorites</button>
+                    {/* Maybe we do gainers and losers */}
+                    <button>Gainers</button>
+                    <button>Losers</button>
+                </div>
+                <div className="column-2-2">
+                    <select name="currency" id="currency">
+                        <option value="usd">USD</option>
+                        <option value="rupee">Rupee</option>
+                        <option value="won">Won</option>
+                    </select>
+                    <select name="timestamp" id="timestamp">
+                        <option value="day">1D</option>
+                        <option value="week">7D</option>
+                        <option value="month">1M</option>
+                        <option value="year">1Y</option>
+                    </select>
+                </div>
             </div>
 
         </div>
