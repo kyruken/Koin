98c1083b7ac55398dc9979519faadfdd907038b1
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-23 13:02:06 -0700

    finish layout of grid

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -9,17 +9,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name,price) {
-    return {name, price};
+function createData(count, name, price, supply) {
+    return {count, name, price, supply};
   }
+  const rows = [];
   
-  const rows = [
-    createData('Bitcoin', '$13,370'),
-    createData('Bitcoin', '$13,370'),
-    createData('Bitcoin', '$13,370'),
-    createData('Bitcoin', '$13,370'),
-    createData('Bitcoin', '$13,370'),
-  ];
+  for (let x = 1; x < 6; x++) {
+    rows.push(createData(x, 'Bitcoin', '$13,370', '19,000'));
+
+  }
+
 
   console.log(StarBorderIcon);
 export default function Grid() {
@@ -29,21 +28,26 @@ export default function Grid() {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align="center" colspan="2">Name</TableCell>
-                        <TableCell align="center">Price</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell>#</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Price</TableCell>
+                        <TableCell>Supply</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                 {rows.map((row) => (
                     <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    key={row.count}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
                     >
-                    <TableCell><StarBorderIcon></StarBorderIcon></TableCell>
+                    <TableCell sx={{border: 0}}><StarBorderIcon></StarBorderIcon></TableCell>
+                    <TableCell sx={{border: 0}}>{row.count}</TableCell>
                     <TableCell component="th" scope="row"  sx={{border: 0}}>
-                        {row.name}
+                    {row.name}
                     </TableCell>
-                    <TableCell align="center" sx={{border: 0}}>{row.price}</TableCell>
+                    <TableCell sx={{border: 0}}>{row.price}</TableCell>
+                    <TableCell sx={{border: 0}}>{row.supply}</TableCell>
 
                     </TableRow>
                 ))}
