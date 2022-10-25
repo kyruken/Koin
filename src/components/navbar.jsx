90307496ffb0af3c825a44b2f0bc081473960395
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-24 18:09:18 -0700

    change styles and add logo to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Navbar() {
     return (
         <div className="navbar">
-            <h1>Koin</h1>
+            <img src="../src/assets/koinlogo.png" width="125" className="margin-left"></img>
+
+            <div className="navbar-links margin-right">
+                <a>Alternatives</a>
+                <a>About Us</a>
+            </div>
             
         </div>
     )
