5bb8bab240623636cde10777e605e2e0ddb74317
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-25 17:03:05 -0700

    add event for typing into searchbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import '../App.css';
 
-import Menu from './menu.jsx';
 export default function Header(props) {
+    const [hasContent, setHasContent] = React.useState(false);
     function handleChange(e) {
         console.log(e.target.value)
-        if(e.target.value === props.allCoins){
-
-        }
+        props.allCoins.forEach(coin => {
+            const formatName = coin.name.toLowerCase();
+            const formatValue = e.target.value.toLowerCase();
+            const isVisible = formatName.includes(formatValue);
+            if (isVisible) {
+                setHasContent(true); 
+                console.log(coin);
+            }
+        })
     }
+
     return (
         <div className="header">
             <h2>What coins are you interested in?</h2>
@@ -18,9 +25,6 @@ export default function Header(props) {
                 <img alt="Magnifying glass icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/256px-Magnifying_glass_icon.svg.png"></img>
             </div>
             
-            <Menu 
-                allCoins={props.allCoins}
-            />
         </div>
     )
 }
