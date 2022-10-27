97931a3f7f0323006c5de535003e4ea738b5a4b6
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-27 09:07:07 -0700

    logo leads to home page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function Navbar() {
     return (
         <div className="navbar">
-            <Link to='/'><img src="../src/assets/koinlogo.png" width="125" className="margin-left"></img></Link>
+            <Link to='/Koin'><img src="../src/assets/koinlogo.png" width="125" className="margin-left"></img></Link>
 
             <div className="navbar-links margin-right">
                 <Link to='/alternatives'>Alternatives</Link>
