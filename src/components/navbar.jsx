67d0cc6fc0f4e9ac108534a41ea69eddf6c5af17
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-27 09:13:33 -0700

    change logo to online src rather than local

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function Navbar() {
     return (
         <div className="navbar">
-            <Link to='/Koin'><img src="../src/assets/koinlogo.png" width="125" className="margin-left"></img></Link>
+            <Link to='/Koin'><img src="https://i.imgur.com/pe8pQ6v.png" width="125" className="margin-left"></img></Link>
 
             <div className="navbar-links margin-right">
                 <Link to='/alternatives'>Alternatives</Link>
